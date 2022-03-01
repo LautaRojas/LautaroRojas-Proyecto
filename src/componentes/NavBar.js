9f54fcece25d6ee280652282abfe56bdb6497949
lautaro <lautarouriel212@gmail.com>
2022-03-01 00:16:08 -0300

    Desafio 2

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+    return <header>
+        <h1>Tienda Discos</h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Discos</a>
+            <a href="#">Carrito</a>
+        </nav>
+    </header>
+        
+
+
+    
+}
+
+export default NavBar
